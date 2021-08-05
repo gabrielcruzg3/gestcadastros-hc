3bf3a72d7453457bcbc1e70c5eedaaa2d672357d
gabrielcruzg3 <cruz.gabrielg3@gmail.com>
2021-08-05 09:39:51 -0300

    limpeza de codigo e refatoramento logica

--- js/cliente.js
+++ js/cliente.js
@@ -9,44 +9,15 @@ function cadastraUser(){
     localStorage.setItem('adress', JSON.stringify(adress))
 }
 
-
-window.onload=function(){
-
-document.getElementById("form-clientes").addEventListener("submit", function () {
-   document.getElementById('form-clientes').submit(cadastraUser());
-   cadastraUser();
- });
-}
-
 function listaUser(){  
-    
-
-    document.getElementById('nomeList').innerHTML = localStorage.getItem('nome', JSON.stringify(nome))   
-    document.getElementById('emailList').innerHTML = localStorage.getItem('email', JSON.stringify(email))
-    document.getElementById('adressList').innerHTML = localStorage.getItem('adress', JSON.stringify(adress))
-}
-
-
-window.onload=function(){
-
-document.getElementById("listaClientes").addEventListener("button", function (e) {
-   document.getElementById('listaClientes').button(listaUser());
-   listaUser();
-   e.preventDefault();
-   
- });
+    document.getElementById('nomeList').innerHTML = localStorage.getItem('nome', nome)   
+    document.getElementById('emailList').innerHTML = localStorage.getItem('email', email)
+    document.getElementById('adressList').innerHTML = localStorage.getItem('adress', adress)
 }
 
 function removeUser(){
     localStorage.removeItem('nome')   
     localStorage.removeItem('email')
     localStorage.removeItem('adress')
-}
-
-window.onload=function(){
-
-document.getElementById("listaClientes").addEventListener("submit", function () {
-   document.getElementById('listaClientes').submit(removeUser());
-   removeUser();   
- });
+    listaUser();
 }
--- js/produto.js
+++ js/produto.js
@@ -1,45 +1,14 @@
 function cadastraProdutos(){
-    var nomeProduto = document.getElementById('nomeProduto').value;
-      
-   localStorage.setItem('nomeProduto', JSON.stringify(nomeProduto))
-}
-
-
-window.onload=function(){
-
-document.getElementById("form-produtos").addEventListener("submit", function () {
-   document.getElementById('form-produtos').submit(cadastraProdutos());
-   cadastraProdutos();
+    var nomeProduto = document.getElementById('nomeProduto').value      
    
- });
+    localStorage.setItem('nomeProduto', JSON.stringify(nomeProduto))
 }
 
 function listaProdutos(){
-    document.getElementById('produtoList').innerHTML = localStorage.getItem('nomeProduto', JSON.stringify(nomeProduto))
-}
-
-
-window.onload=function(){
-
-document.getElementById("produtoList").addEventListener("button", function (e) {
-   document.getElementById('produtoList').button(listaProdutos());
-   listaProdutos();
-   e.preventDefault();
-   
- });
+    document.getElementById('produtoList').innerHTML = localStorage.getItem('nomeProduto', nomeProduto)
 }
 
 function removeProdutos(){
-
    localStorage.removeItem('nomeProduto')
-}
-
-
-window.onload=function(){
-
-document.getElementById("listaProdutos").addEventListener("remove", function () {
-   document.getElementById('listaProdutos').remove(removeProdutos());
-   removeProdutos();
-   
- });
+   listaProdutos();
 }
